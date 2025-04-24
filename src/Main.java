4ca619fd3101d8c2e2e2f0698ad4b4e345784d8b
lstolinov <lstolinova@seznam.cz>
2025-04-24 08:03:15 +0200

    výměra pozemku

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        int vymera = 55;
+        int cenaZaMetr = 150;
+        String nazev="Sad Pálava";
+        int cena=vymera*cenaZaMetr;
+        System.out.println("Cena pozemku je "+ cena);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
         }
     }
-}
+
